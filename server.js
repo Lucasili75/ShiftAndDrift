924b011a6ce1e3251f508faba9207ecfd1cf96db
Lucasili75 <115477436+Lucasili75@users.noreply.github.com>
2025-06-16 17:49:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -70,14 +70,14 @@ app.get('/check-and-notify', async (req, res) => {
     notification = {
       title: 'Nuovo giocatore!',
       body: `${player} si è aggiunto alla gara`,
-      click_action: 'GAME_ACTIVITY'
+      click_action: 'WAIT_ACTIVITY'
     };
   }
   if (fun === 'deletePlayer' && player) {
     notification = {
       title: 'Giocatore uscito!',
       body: `${player} ha abbandonato la gara`,
-      click_action: 'GAME_ACTIVITY'
+      click_action: 'WAIT_ACTIVITY'
     };
   }
 
