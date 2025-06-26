50aa2cb447bcaab34e36f78d32c98e365494bab2
Lucasili75 <115477436+Lucasili75@users.noreply.github.com>
2025-06-26 18:16:22 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Inserisci le tue chiavi reali qui
+const firebaseConfig = {
+  apiKey: "AIzaSyAwSsoQSRoFH0DBtMDEG75CL9Z6-oRhZ00",
+  authDomain: "shiftanddrift.firebaseapp.com",
+  databaseURL: "https://shiftanddrift-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "shiftanddrift",
+  storageBucket: "shiftanddrift.appspot.com",
+  messagingSenderId: "332080935366",
+  appId: "1:332080935366:android:23467af08e9139644d02c7"
+};
+
+firebase.initializeApp(firebaseConfig);
